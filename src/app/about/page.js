eb14bd6c63767ca5c0f9ccaa76000a9b929f20ed
รพีภัทร ตาสุศรี <akaoliroom.01@gmail.com>
2024-08-07 08:28:36 +0700

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -8,9 +8,6 @@ export default function About() {
         <figcaption className="figure-caption"></figcaption>
       </figure>
     </div>
-  export default function About() {
-   return (
-  <div className="container-fluid">
       <figure className="figure">
       <Image src="/assets/img/about1.png" layout="responsive" className="card-img-top" width={100} height={100} alt="..." />
         <figcaption className="figure-caption"></figcaption>
